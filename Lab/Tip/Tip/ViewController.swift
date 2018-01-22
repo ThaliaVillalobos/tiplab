cac1e17a85e54083be493b8e1ae7b9b4588a136e
ThaliaVillalobos <tvillalobos@csumb.edu>
2018-01-22 15:52:11 -0800

    Add files via upload
    layout of tip

---
+++ Lab/Tip/Tip/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Thalia Villalobos on 1/22/18.
+//  Copyright © 2018 Thalia Villalobos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        print("Hello")
+    }
+
+}
+
